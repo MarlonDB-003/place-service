7586fa5cf9e17bbb226b24b7c2c49e3ded65b97b
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-14 14:35:29 -0400

    create project place-serviice

---
+++ pacle-service/src/main/java/br/com/marlonsilva/pacleservice/PacleServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.marlonsilva.pacleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacleServiceApplication.class, args);
+	}
+
+}
---
+++ pacle-service/src/test/java/br/com/marlonsilva/pacleservice/PacleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonsilva.pacleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
